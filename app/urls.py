fc0b69d1f4e5c2581f43be718cdcd5a931a930ac
KoSafronov <kostya.safronov@gmail.com>
2024-06-15 01:13:07 +0300

    hw4

---
+++ app/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('hot/', views.hot, name='hot'),
+    path('hot/', views.hot_questions, name='hot_questions'),
+    path('login/', views.login, name='login'),
+    # path('login/', views.logout, name='logout'),
+    path('ask/', views.ask, name='ask'),
+    path('settings/', views.settings, name='settings'),
+    path('register/', views.register, name='register'),
+
+    path('questions/<int:question_id>', views.question, name='question'),
+    path('question/<int:question_id>/answer', views.answer, name='answer'),
+    path('answer/correct', views.correct, name='correct'),
+    path('tags/<tag_name>', views.tag, name='tag'),
+    path('404/', views.oops_404, name='oops'),
+
+    path('ask/', views.ask_question, name='ask_question'),
+    path('search/', views.search, name='search'),
+    path('ask/<str:query>/', views.ask_question, name='ask_question_with_query'),
+]
