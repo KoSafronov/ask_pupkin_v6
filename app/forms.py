732f8b890d2dc83f0208841f96ffeaa6b7297412
KoSafronov <kostya.safronov@gmail.com>
2024-06-15 01:31:01 +0300

    new commit

--- app/forms.py
+++ app/forms.py
@@ -99,46 +99,6 @@ class ProfileForm(forms.ModelForm):
 
         return user
 
-# class AskForm(forms.ModelForm):
-#     tags = forms.CharField(required=False)
-#
-#     def __init__(self, user, *args, **kwargs):
-#         self.user = user
-#         super().__init__(*args, **kwargs)
-#
-#     class Meta:
-#         model = models.Question
-#         fields = ['title', 'content']
-#
-#     def clean_tags(self):
-#         tags = self.cleaned_data.get('tags')
-#         if not re.match(r'^[a-zA-Z\-_ ]+$', tags):
-#             raise forms.ValidationError('A tag name can only consist of letters and dashes.')
-#
-#         return tags.split()
-#
-#     @staticmethod
-#     def add_tags_to_question(question, tags):
-#         for tag in tags:
-#             tag, created = models.Tag.objects.get_or_create(name=tag)
-#             question.tags.add(tag)
-#
-#     def save(self, commit=True):
-#         question = models.Question(title=self.cleaned_data.get('title'),
-#                                    content=self.cleaned_data.get('content'))
-#         question.user = self.user
-#
-#         question.user.profile.rating += NEW_QUESTION_AWARD
-#
-#         if commit:
-#             question.save()
-#             self.user.profile.save()
-#             AskForm.add_tags_to_question(question, self.cleaned_data.get('tags'))
-#
-#         return question
-
-
-
 
 
 
@@ -181,49 +141,6 @@ class AskForm(forms.ModelForm):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class AnswerForm(forms.ModelForm):
-#     class Meta:
-#         model = models.Answer
-#         fields = ['content']
-#
-#     def __init__(self, user, question, *args, **kwargs):
-#         self.user = user
-#         self.question = question
-#         super().__init__(*args, **kwargs)
-#
-#     def save(self, commit=True):
-#         answer = models.Answer(content=self.cleaned_data.get('content'))
-#         answer.user = self.user
-#
-#         question = self.question
-#         question.answers_count += 1
-#
-#         answer.question = question
-#         answer.is_correct = False
-#
-#         answer.user.profile.rating += NEW_ANSWER_AWARD
-#
-#         if commit:
-#             answer.save()
-#             question.save()
-#             self.user.profile.save()
-#
-#         return answer
 
 
 class AnswerForm(forms.ModelForm):
@@ -367,46 +284,6 @@ class VoteForm(forms.Form):
 
         return rating
 
-#
-# class RegisterForm(forms.ModelForm):
-#     username = forms.CharField(min_length=3, max_length=30)
-#     email = forms.EmailField(min_length=6, max_length=30)
-#     password = forms.CharField(widget=forms.PasswordInput)
-#     password_confirm = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
-#
-#     class Meta:
-#         model = models.User
-#         fields = ['username', 'email', 'password']
-#
-#     def clean_email(self):
-#         email = self.cleaned_data.get('email')
-#         if email and models.User.objects.filter(email=email).exists():
-#             raise forms.ValidationError('A user with that email already exists.')
-#
-#         return email
-#
-#     def clean_password_confirm(self):
-#         password = self.cleaned_data.get('password')
-#         password_confirm = self.cleaned_data.get('password_confirm')
-#         if password != password_confirm:
-#             raise forms.ValidationError('Passwords do not match.')
-#
-#         return password_confirm
-#
-#     def save(self, commit=True):
-#         user = models.User(
-#             username=self.cleaned_data['username'],
-#             email=self.cleaned_data['email'],
-#         )
-#         user.set_password(self.cleaned_data['password'])
-#         profile = models.Profile(user=user)
-#
-#         if commit:
-#             user.save()
-#             profile.save()
-#
-#         return user
-
 
 from django import forms
 from django.contrib.auth.models import User
@@ -431,98 +308,6 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
 
-# class UserSettingsForm(forms.ModelForm):
-#     email = forms.EmailField(min_length=6, max_length=30, required=False)
-#     username = forms.CharField(min_length=3, max_length=30, required=False)
-#     avatar = forms.ImageField(required=False)
-#
-#     def clean_email(self):
-#         email = self.cleaned_data.get('email')
-#         if email and models.User.objects.filter(email=email).exists():
-#             raise forms.ValidationError('A user with that email already exists.')
-#
-#         return email
-#
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'avatar']
-#
-#     def __init__(self, user, *args, **kwargs):
-#         self.user = user
-#         super().__init__(*args, **kwargs)
-#
-#     def save(self, commit=True):
-#         user = super(UserSettingsForm, self).save(commit=False)
-#         avatar = self.cleaned_data.get('avatar')
-#
-#         if avatar:
-#             user.profile.avatar = avatar
-#         if commit:
-#             user.save()
-#
-#             user.profile.save()
-#             profile.username = self.cleaned_data['username']
-#             if self.cleaned_data['avatar']:
-#                 profile.avatar = self.cleaned_data['avatar']
-#             profile.save()
-#         return user
-
 
 from django import forms
 from django.contrib.auth.models import User
-
-# class UsernameChangeForm(forms.ModelForm):
-#     class Meta:
-#         model = models.User
-#         fields = ['username']
-#
-#     def clean_username(self):
-#         username = self.cleaned_data.get('username')
-#         if username and models.User.objects.filter(username=username).exists():
-#             raise forms.ValidationError("This username is already taken.")
-#         return username
-#
-#     def save(self, commit=True):
-#         user = models.User(
-#             username=self.cleaned_data['username'],
-#             )
-#
-#         profile = models.Profile(user=user)
-#
-#         if commit:
-#             user.save()
-#             profile.save()
-#
-#         return user
-
-
-# class RegForm(forms.ModelForm):
-#     password = forms.CharField(label='Password',
-#                                widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password'}))
-#     confirm_password = forms.CharField(label='Confirm Password',
-#                                        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm your password'}))
-#     nickname = forms.CharField(label='Nickname', widget=forms.TextInput(attrs={'placeholder': 'Enter your nickname'}))
-#     avatar = forms.ImageField(label='Avatar', required=False)
-#
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email', 'password')
-#
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         password = cleaned_data.get("password")
-#         confirm_password = cleaned_data.get("confirm_password")
-#
-#         if password != confirm_password:
-#             raise forms.ValidationError("Passwords must match")
-#
-#     def save(self, commit=True):
-#         user = super().save(commit=False)
-#         user.set_password(self.cleaned_data['password'])
-#         user.save()
-#         profile = Profile.objects.create(user=user)
-#         profile.avatar = self.cleaned_data['avatar']
-#         profile.name = self.cleaned_data['nickname']
-#         profile.save()
-#         return user
-
